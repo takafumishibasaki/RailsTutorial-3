cee56112c8992e71df4e5c65271dee223ac3aa3d
Takafumi Shibasaki <tkfmsb@gmail.com>
2015-12-11 16:01:18 +0900

    Finish static pages

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module RailsTutorial3
   class Application < Rails::Application
+    config.generators.template_engine = :slim
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

1bfd346136b829ce6b8f8a031ef1e2aef9e07e2c
Takafumi Shibasaki <tkfmsb@gmail.com>
2015-12-16 18:22:40 +0900

    Finish user signup

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,9 @@
 
 FactoryGirl.define do
   factory :user do
-    name "MyString"
-    email "MyString"
+    name "Michael Hartl"
+    email "michael@example.com"
+    password "foobar"
+    password_confirmation "foobar"
   end
 end
